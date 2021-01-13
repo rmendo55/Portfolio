f98cc1e72caf072782e9a7c346e75e6beb63038c
rmendo55 <mendozarafael914@gmail.com>
2021-01-13 10:45:53 -0800

    click function for image gallery

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ $(document).ready(function() {
     $(".toggle-button").click(() => {
             $("div.navbar-links").toggleClass("active");
     });
+
+    $("img-link").click(() => {
+        $("project-img").toggleClass("active");
+    });
 });
 
